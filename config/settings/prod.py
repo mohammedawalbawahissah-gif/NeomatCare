9c92f0f757d8a5e96f6bc71832bee84278c2d663
Mohammed Awal Bawah Issah <mohammedawalbawahissah@gmail.com>
2026-05-19 03:18:11 +0000

    fix: add CORS_ALLOWED_ORIGINS for dashboard

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,26 +2,33 @@ from .base import *
 
 DEBUG = False
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "json": {
-            "format": '{"time":"%(asctime)s","level":"%(levelname)s","message":"%(message)s"}',
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'json': {
+            'format': '{\"time\":\"\%(asctime)s\",\"level\":\"\%(levelname)s\",\"message\":\"\%(message)s\"}',
         }
     },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "json",
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'json',
         }
     },
-    "root": {"handlers": ["console"], "level": "INFO"},
+    'root': {'handlers': ['console'], 'level': 'INFO'},
 }
